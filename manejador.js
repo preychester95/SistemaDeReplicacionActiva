024b2953c3bab0d9b1e01e5139fe02df8438bd6f
Adrian Errea <adrianerrea>
2018-10-26 17:59:37 +0200

    Manejador_v2

--- manejador.js
+++ manejador.js
@@ -9,25 +9,31 @@ var LastServerReq=0;
 var seq;
 //Variables del sequenciador
 //Hay que ver cual de las dos siguentes funciona
-//EN EL ARRAY SEQUENCED SE GUARDAN LOS ID'S DE LAS PETICIONES????
-var Sequenced=new Array();
-var Sequenced=[];
-var LocalSeq=1;
+//En el array sequenced se guarda el json
+var Sequenced={}
+var Requests={}
+var Replicas=['RR_1','RR_2','RR_3'];
 
-function getSeq(requestARH){
-		i=1;
-		while(Sequenced[i]!=requestARH){
+var LocalSeq=1;
+var UltimaReq=0;
+
+/*function getSeq(requestARH){
+		i=0;
+		while(Sequenced[i].id!=requestARH.id){
 			i=i+1;
 		}
-		if(Sequenced[i]!=requestARH){
-			handler_TO.send(requestARH);
+		if(Sequenced[i].id!=requestARH.id){
+			handler_TO.send(JSON.stringify(requestARH));	
+			Requests[UltimaReq]=JSON.parse(requestARH);		
 		}else{
 			return i;
 		}
-}
-function getReq(j){
+}*/
+
+/*function getReq(j){
 	return Sequenced[j];
-}
+}*/
+
 //RR
 var portRR = process.argv[2];
 //Replicas
@@ -38,23 +44,32 @@ var portTO=process.argv[4];
 handler_RR.bind('tcp://*:' + portRR);
 handler_FO.bind('tcp://*:' + portFO);
 handler_TO.bind('tcp://*'+ portTO);
+numberReplicas=Replicas.length;
 console.log('Esperando...');
 
 //Cuando nos devuelve el TO
 handler_TO.on('message',function(requestTO){
 	//Get the JSON send by a TO
-  	var request_parsedTO = JSON.parse(requestTO); 
-	i=1;
+  	var request_parsedTO = JSON.parse(requestTO);
+  	var request_id=request_parsedTO.idClient; 
+	/*i=0;
 	while(Sequenced[i]!=requestTO){
 		i=i+1;
-	}
-	if(Sequenced[i]!=requestTO){
+	}*/
+	if (Sequenced.indexOf(request_parsedTO)==-1){
 		Sequenced[LocalSeq]=requestTO;
 		LocalSeq=LocalSeq+1;
+
 	}
-	if(requestTO=requestARH){
-		//DEVOLVER AL getSeq el valor de la posicion???????????????????''
+
+	/*j=0;
+	while(requestARH[j].idClient!=request_id){
+		j=j+1;
 	}
+	if(requestARH[j].idClient==request_id){
+		
+	}*/
+	
 });
 
 
@@ -62,21 +77,42 @@ handler_TO.on('message',function(requestTO){
 handler_RR.on('message', function(request) {
 	//Get the JSON send by a client.
   	var request_parsed = JSON.parse(request); 
-	//Se le pasa la peticion. HAY QUE PASARLE TODA ENTERA?????
-	seq=getSeq(request);
+  	var request_id=request_parsed.idClient; 
+  	var request_op=request_parsed.msg.op;
+  	var request_arg=request_parsed.msg.arg;
+
+  	//Codigo manejador
+
+  //	i=0;
+	//while(Sequenced[i].id!=requestARH.id){
+	//	i=i+1;
+	//}
+	//Si no existe 
+	if (Sequenced.indexOf(request_parsed)==-1){
+		handler_TO.send(request);
+
+	}else{
+		seq=Sequenced.indexOf(request_parsed);
+	}
+	/*if(Sequenced[i].id!=requestARH.id){
+		handler_TO.send(JSON.stringify(requestARH));	
+		Requests[UltimaReq]=JSON.parse(requestARH);		
+	}else{
+		return i;
+	}*/
+
+	//seq=getSeq(request_id);
 	if (seq>LastServerReq+1){
 		for (let j=LastServerReq;j<seq;j++){
-			req=getReq(j);
-			//COMO SABEMOS EL NUMERO DE REPLICAS?????
+			//req=getReq(j);
+			req=Sequenced[j];
 			for(let r=1;r<numberReplicas;r++){
-				handler_FO.send(r,j,request);
+				handler_FO.send(r,j,req);
 			}
 		}
 	}
 	for(let r=1;r<numberReplicas;r++){
-		request = JSON.parse(request);
-		seq = JSON.parse(seq);
-		handler_FO.send(r,seq,request);
+		handler_FO.send(r,seq,req);
 	}
 	LastServerReq=Math.max(LastServerReq,seq);
 });
@@ -86,7 +122,11 @@ handler_FO.on('message', function(reply) {
 		reply = JSON.parse(reply);
 		msg='recibido de FO';
 		//HAY QUE COMPROBAR SI TIENE EL MISMO NUMERO DE SECUENCIA Y SI LO HEMOS RECIBIDO ANTES DE OTRA FO????????????????
-		i=1;
+		if(reply.seq==seq){
+			handler_RR.send(reply,msg);
+
+		}
+		/*i=1;
 		while(Sequenced[i]!=reply){
 			i=i+1;
 		}
@@ -96,7 +136,7 @@ handler_FO.on('message', function(reply) {
 			if (reply=LastServerReq){
 				handler_RR.send(reply,msg);
 			}
-		}
+		}*/
 });
 
 
