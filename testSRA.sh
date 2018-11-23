d0bca995ae6071870b265932a7ed3a616e74f1a7
Iosu <iosu-rodri@hotmail.com>
2018-11-23 16:45:11 +0100

    Titulos para los terminales

--- testSRA.sh
+++ testSRA.sh
@@ -2,17 +2,17 @@
 NUMHIJOS=$1
 echo "El numero de procesos es: "$NUMHIJOS
 	
-gnome-terminal --tab -e "node router-router-RRToHandler.js 9000 9001 S" 
-gnome-terminal --tab -e  "node router-router-HandlerToReplica.js 9002 9003 S" 
+gnome-terminal --tab --title="Router: RRToHandler" -e "node router-router-RRToHandler.js 9000 9001 S" 
+gnome-terminal --tab --title="Router: HandlerToReplica" -e  "node router-router-HandlerToReplica.js 9002 9003 S" 
 cd Prepare_scripts/
-gnome-terminal --tab -e "node prepare_Replicas.js 3"
+gnome-terminal --tab --title="Replicas" -e "node prepare_Replicas.js 3"
 sleep 1
-gnome-terminal --tab -e "node prepare_handlers.js 10"
+gnome-terminal --tab --title="Manejadores" -e "node prepare_handlers.js 10"
 sleep 1
 cd ..
-gnome-terminal --tab -e "node TO_module.js 5000"
+gnome-terminal --tab --title="TotalOrder" -e "node TO_module.js 5000"
 sleep 1
 cd Prepare_scripts/
-gnome-terminal --tab -e "node prepare_RRs.js 30 9000"
+gnome-terminal --tab --title="RRs" -e "node prepare_RRs.js 30 9000"
 sleep 1
-gnome-terminal --tab -e "node prepare_clients.js"
+gnome-terminal --tab --title="Clientes" -e "node prepare_clients.js"
