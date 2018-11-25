17cd6c280d97df316bb697f553a11dcf0a2cdd66
Iosu <iosu-rodri@hotmail.com>
2018-11-25 13:55:14 +0100

    No estaba el fichero de test en el repositorio

---
+++ testSRA.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+NUMHIJOS=$1
+echo "El numero de procesos es: "$NUMHIJOS
+	
+gnome-terminal --tab --title="Router: RRToHandler" -e "node router-router-RRToHandler.js 9000 9001 S" 
+gnome-terminal --tab --title="Router: HandlerToReplica" -e  "node router-router-HandlerToReplica.js 9002 9003 S" 
+cd Prepare_scripts/
+gnome-terminal --tab --title="Replicas" -e "node prepare_Replicas.js 1"
+sleep 1
+gnome-terminal --tab --title="Manejadores" -e "node prepare_handlers.js 3"
+sleep 1
+cd ..
+gnome-terminal --tab --title="TotalOrder" -e "node TO_module.js 5000"
+sleep 1
+cd Prepare_scripts/
+gnome-terminal --tab --title="RRs" -e "node prepare_RRs.js 4 9000"
+sleep 1
+gnome-terminal --tab --title="Clientes" -e "node prepare_clients.js"
